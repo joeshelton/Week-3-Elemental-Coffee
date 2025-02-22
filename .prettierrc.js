62afbc7c4f0375977cf3a774d554569e8548b11e
Joe <joe.shelton@cox.net>
2025-02-22 13:34:12 -0600

    Elemental Coffee Blog
    Added blog page

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    htmlWhitespaceSensitivity: 'ignore',
+    tabWidth: 4,
+    useTabs: false,
+    printWidth: 80,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+};
